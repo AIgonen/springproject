c9b6ecbbeacdd80773136f13e1715eaeed7de459
Andrei <a.igonen>
2023-05-30 13:13:32 +0300

    Updated main responce added

--- src/main/java/com/bpw/Main.java
+++ src/main/java/com/bpw/Main.java
@@ -1,11 +1,35 @@
 package com.bpw;
 
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.bpw")
+//@EnableAutoConfiguration
+@RestController
 public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+    public static void main(String[] args) {SpringApplication.run(Main.class, args);}
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "C#"),
+                new Person("Alex", 30, 50000)
+        );
+
+    }
+    record Person(String name, int age, double savings) {
+
+    }
+    record GreetResponse (String greet, List<String> languages, Person person) {
+
     }
 }
